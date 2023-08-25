321146ddda1bd3d775b4913a562f0374a7af316d
Surya Faturohman <suryafaturohman@gmail.com>
2023-08-25 17:07:14 +0700

    Create register & login facility

--- app.js
+++ app.js
@@ -1,49 +1,22 @@
-const express = require('express');
-const { MongoClient } = require('mongodb')
+const express = require('express')
+const databaseMiddleware = require('./middleware/databaseMiddleware.js')
+const authRouter = require('./routes/auth-route.js')
 
 const app = express();
 
 app.use(express.json())
 
-let db
-(async () => {
-    try {
-        const client = await new MongoClient('mongodb://127.0.0.1:27017').connect()
-        db = client.db('revouweek10')
-    } catch (error) {
-        console.log(error, `<=================== error ==================`);
-    }
-})()
-
-app.post('/v1/users', async (req, res) => {
-    console.log(req.body, `<=================== body ==================`);
-
-    const { username, email } = req.body
-
-    const user = await db.collection('users').insertOne({ username, email })
-
-    res.status(200).json({
-        message: 'success',
-        data: user
-    })
-})
-
-app.get('/v1/users', async (req, res) => {
-    const users = await db.collection('users').find().toArray()
-
-    res.status(200).json({
-        message: 'success',
-        data: users
-    })
-
-})
+app.use(databaseMiddleware)
 
 app.get('/', (req, res) => {
-    res.send('My App')
+    res.send('Transfer Request Managament API')
 })
+
+app.use('/v1/auth/', authRouter)
+app.use('/v1/transfer/', () => { })
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Running on port http://localhost:${port}`)
+    console.log(`Server is running on port http://localhost:${port}`)
 })
---
+++ controller/auth-controller.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const register = async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        const user = await req.db.collection('users').findOne({ username })
+
+        if (user) {
+            throw new Error('Username already exists')
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await req.db.collection('users').insertOne({ username, password: hashedPassword })
+        res.status(200).json({
+            message: 'User successfully registered',
+            data: newUser
+        })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+const login = async (req, res) => {
+    const { username, password } = req.body
+    const user = await req.db.collection('users').findOne({ username })
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if (isPasswordCorrect) {
+        const token = jwt.sign({ username: user.username, id: user._id }, 'sign')
+        res.status(200).json({
+            message: 'User successfully logged in',
+            data: token
+        })
+    } else {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+module.exports = {
+    register,
+    login
+}
