d5f02b921f7acb136901d396ea1f428455fff98d
Surya Faturohman <suryafaturohman@gmail.com>
2023-08-26 06:59:41 +0700

    Change to deployed database

--- middleware/databaseMiddleware.js
+++ middleware/databaseMiddleware.js
@@ -2,7 +2,7 @@ const { MongoClient, ObjectId } = require('mongodb')
 
 const databaseMiddleware = async (req, res, next) => {
     try {
-        const client = await new MongoClient('mongodb://127.0.0.1:27017').connect()
+        const client = await new MongoClient('mongodb://mongo:Ko7S8NCW13W5gVsDKw2F@containers-us-west-110.railway.app:7655').connect()
         db = client.db('revouweek10')
 
         req.db = db
